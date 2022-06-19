644589fa96ea2de4ea23f9be7c73508755865904
Johannes Preisinger <cnnwarthem@gmail.com>
2022-06-19 12:04:35 +0200

    Support multilingual rsvp forms

--- src/components/rsvp.vue
+++ src/components/rsvp.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="page">
     <br>
-    <iframe
+    <iframe v-if="($i18n.locale === 'Türkçe')"
+        :src="$config.FORM_TR"
+        width="100%" height="1300" frameborder="0" marginheight="0" marginwidth="0">Loading…
+    </iframe>
+    <iframe v-else
         :src="$config.FORM_EN"
         width="100%" height="1300" frameborder="0" marginheight="0" marginwidth="0">Loading…
     </iframe>
@@ -12,7 +16,6 @@
 export default {
   name: "rsvp",
   mounted() {
-    console.log(this.$config.FORM_EN)
   }
 }
 </script>
