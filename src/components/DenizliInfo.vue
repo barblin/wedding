61f4c82dc84a3d0eda7af5b6d8d588aa111976a9
Johannes Preisinger <cnnwarthem@gmail.com>
2022-08-01 01:02:05 +0200

    additional infos and attractions

--- src/components/DenizliInfo.vue
+++ src/components/DenizliInfo.vue
@@ -2,10 +2,14 @@
   <div>
     <h3>{{ $t("message.tips") }}</h3>
     <div class="fact-head mb-1">
-      <p>{{ $t("message.sunscreen") }}</p>
-      <p>{{ $t("message.badehose") }}</p>
-      <p>{{ $t("message.denizliText") }}</p>
-      <p>{{ $t("message.shorts") }}</p>
+      <p>&#8611; {{ $t("message.sunscreen") }}</p>
+      <p>&#8611; {{ $t("message.badehose") }}</p>
+      <p>&#8611; {{ $t("message.shorts") }}</p>
+      <p>&#8611; {{ $t("message.shorts") }}</p>
+      <p>&#8611; {{ $t("message.exchange") }} <a href="https://www.google.com/maps/search/D%C3%B6viz+Ofis+Denizli/@37.7857086,29.0778137,14.75z">
+        Location
+      </a></p>
+      <p>&#8611; {{ $t("message.denizliText") }}</p>
     </div>
     <br>
     <br>
@@ -14,37 +18,78 @@
     <div class="cards-page">
       <div class="card card--mr">
         <img
-          src="pamukkale-white-beauty-1394441-1919x1274.jpg"
-          alt="Avatar"
-          style="width:100%"
+            src="pamukkale-white-beauty-1394441-1919x1274.jpg"
+            alt="Avatar"
+            style="width:100%"
         />
         <div class="container">
           <h4>
             <b>{{ $t("message.denizliSprings") }}</b>
           </h4>
-          <p>{{ $t("message.denizliSprings") }}</p>
+          <p><a href="https://goo.gl/maps/y1EyEJqDZkXmuKW9A">{{ $t("message.denizliSpringsLink") }}</a></p>
         </div>
       </div>
       <div class="card card--mr">
-        <img src="ruins.jpg" alt="Avatar" style="width:100%" />
+        <img src="ruins.jpg" alt="Avatar" style="width:100%"/>
         <div class="container">
           <h4>
             <b>{{ $t("message.denizliRoman") }}</b>
           </h4>
-          <p>{{ $t("message.denizliRoman") }}</p>
+          <p><a href="https://goo.gl/maps/Y81m433gg46aSX5n7">{{ $t("message.denizliRomanLink") }}</a></p>
         </div>
       </div>
-      <div class="card">
+      <div class="card card--mr">
         <img
-          src="aydin-hassan-rnOdgU4EEWU-unsplash.jpg"
-          alt="Avatar"
-          style="width:100%"
+            src="aydin-hassan-rnOdgU4EEWU-unsplash.jpg"
+            alt="Avatar"
+            style="width:100%"
         />
         <div class="container">
           <h4>
             <b>{{ $t("message.denizliGöl") }}</b>
           </h4>
-          <p>{{ $t("message.denizliGöl") }}</p>
+          <p><a href="https://goo.gl/maps/5WGLgTtyfxofUysP7">{{ $t("message.denizliRomanLink") }}</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="cards-page">
+      <div class="card card--mr">
+        <img
+            src="laodiceia.jpg"
+            alt="Avatar"
+            style="width:100%"
+        />
+        <div class="container">
+          <h4>
+            <b>{{ $t("message.laodicia") }}</b>
+          </h4>
+          <p><a href="https://goo.gl/maps/qaQgtHM92QZks2m36">{{ $t("message.laodiciaLink") }}</a></p>
+        </div>
+      </div>
+      <div class="card card--mr">
+        <img
+            src="ruins.jpg"
+            alt="Avatar"
+            style="width:100%"
+        />
+        <div class="container">
+          <h4>
+            <b>{{ $t("message.tripolis") }}</b>
+          </h4>
+          <p><a href="https://goo.gl/maps/fEtX9rxY62g7Fvbn6">{{ $t("message.tripolisLink") }}</a></p>
+        </div>
+      </div>
+      <div class="card card--mr">
+        <img
+            src="cablecar.jpg"
+            alt="Avatar"
+            style="width:100%"
+        />
+        <div class="container">
+          <h4>
+            <b>{{ $t("message.teleferik") }}</b>
+          </h4>
+          <p><a href="https://goo.gl/maps/fEtX9rxY62g7Fvbn6">{{ $t("message.teleferikLink") }}</a></p>
         </div>
       </div>
     </div>
